0c10421344e1559ad8987fc2fac120878a6bf7f7
Omkesh <shmokedev@gmail.com>
2025-05-18 04:25:37 +0530

    update logger

--- lib/utilities/custom_logger.dart
+++ lib/utilities/custom_logger.dart
@@ -2,10 +2,28 @@ import 'package:logger/logger.dart';
 
 Logger customLogger() {
   return Logger(
-    printer: PrettyPrinter(
-      noBoxingByDefault: true,
-      methodCount: 0,
-      lineLength: 50,
-    ),
+    printer: customPrinter(),
   );
+}
+
+class customPrinter extends LogPrinter {
+  final PrettyPrinter _defaultPrinter = PrettyPrinter(
+    methodCount: 0,
+    noBoxingByDefault: true,
+    lineLength: 50,
+  );
+  final PrettyPrinter _errorPrinter = PrettyPrinter(
+    methodCount: 2,
+    noBoxingByDefault: true,
+    lineLength: 50,
+  );
+
+  @override
+  List<String> log(LogEvent event) {
+    if (event.level == Level.error) {
+      return _errorPrinter.log(event);
+    } else {
+      return _defaultPrinter.log(event);
+    }
+  }
 }
