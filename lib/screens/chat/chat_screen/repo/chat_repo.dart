f80535ced75bf1a45c5b1755ac1c35dad9c41c66
Omkesh <shmokedev@gmail.com>
2025-05-18 04:42:40 +0530

    add logging to chat_repo.dart

--- lib/screens/chat/chat_screen/repo/chat_repo.dart
+++ lib/screens/chat/chat_screen/repo/chat_repo.dart
@@ -1,16 +1,11 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
-import 'package:win32/win32.dart';
 import 'package:zineapp2023/backend_properties.dart';
-import 'package:zineapp2023/database/database.dart';
-import 'package:zineapp2023/models/message.dart';
 import 'package:http/http.dart' as http;
 import 'package:zineapp2023/models/message_response_model.dart';
 import 'package:zineapp2023/models/newUser.dart';
 
 import '../../../../models/events.dart';
 import '../../../../models/rooms.dart';
-import '../../../../models/user.dart';
 import 'package:logger/logger.dart';
 
 var logger = Logger(
@@ -18,9 +13,9 @@ var logger = Logger(
 );
 
 class ChatRepo {
-//=====================================================NEWER CODE================================================================//
+  //=====================================================NEWER CODE================================================================//
 
-//------------------------------------Fetching_all_messages_through_RoomId-------------------------------------------//
+  //------------------------------------Fetching_all_messages_through_RoomId-------------------------------------------//
   Future<List<MessageResponseModel>> getChatMessages(String tempRoomId) async {
     try {
       Uri url = BackendProperties.roomMessageUri(tempRoomId);
@@ -28,7 +23,7 @@ class ChatRepo {
           await http.get(url, headers: BackendProperties.getHeaders());
       if (response.statusCode == 200) {
         final List<dynamic> jsonResponse = jsonDecode(response.body);
-        // logger.d(jsonResponse);
+        logger.d(jsonResponse);
         List<MessageResponseModel> messages = jsonResponse
             .where((json) => json != null && json is Map<String, dynamic>)
             .map((json) =>
@@ -49,8 +44,6 @@ class ChatRepo {
 
   Future<List<Rooms>?> fetchRooms(String email) async {
     Uri url = BackendProperties.roomDataUri(email);
-    // 'http://172.20.10.4:8080/rooms/user'
-    //     '?email=$email';
     final response =
         await http.get(url, headers: BackendProperties.getHeaders());
 
@@ -58,29 +51,17 @@ class ChatRepo {
       final List<dynamic> jsonResponse = jsonDecode(response.body);
       List<Rooms> roomData =
           jsonResponse.map((json) => Rooms.fromJson(json)).toList();
-      // logger.d(jsonResponse);
-      // roomData.forEach((room) {
-      //   print("Room ID: ${room.id}");
-      //   print("Room Name: ${room.name}");
-      //   print("Room Description: ${room.description}");
-      //   print("Room Type: ${room.type}");
-      //   print("Room DP URL: ${room.dpUrl}");
-      //   print("Timestamp: ${room.timestamp}");
-      //   print("Last Message Timestamp: ${room.lastMessageTimestamp}");
-      //   print("Unread Messages: ${room.unreadMessages}");
-      //   print("User Last Seen: ${room.userLastSeen}");
-      //   print("=====================================");
-      // });
+      logger.d(jsonResponse);
+
       return roomData;
     } else {
-      print("failed to load the rooms info :${response.statusCode}");
+      logger.w("Failed to load the rooms info: ${response.statusCode}");
       return [];
     }
   }
 
   //---------------------------------Announcement_details----------------------------//
   Future<List<Rooms>> fetchAnnouncement(String emailId) async {
-    // print("inside Announcement");
     List<Rooms> announcements = [];
     try {
       Uri url = BackendProperties.announcementUri(emailId);
@@ -91,48 +72,43 @@ class ChatRepo {
         if (responseData.containsKey('announcementRoom')) {
           final Map<String, dynamic> announcement =
               responseData['announcementRoom'];
-          print("\n\nannouncement data as:${announcement}");
+          logger.d("Announcement data: $announcement");
           Rooms announcement1 = Rooms.fromJson(announcement);
           announcements.add(announcement1);
         }
       }
     } catch (e) {
-      print("An error occurred while fetching announcement: $e");
+      logger.e("An error occurred while fetching announcement: $e");
     }
     return announcements;
   }
 
   //--------------------------------check LastSeen----------------------------------//
   Future<LastSeen?> fetchLastSeen(String emailId, String roomId) async {
-    // print("inside fetchLastSeen");
-    // print("email: $emailId and roomId: $roomId");
-
     try {
       Uri url = BackendProperties.lastSeenUri(emailId, roomId);
       final response =
           await http.get(url, headers: BackendProperties.getHeaders());
 
-      print("response status code: ${response.statusCode}");
+      logger.d("Response status code: ${response.statusCode}");
       if (response.statusCode == 200) {
         final Map<String, dynamic> responseData = json.decode(response.body);
         if (responseData.containsKey('info')) {
           final Map<String, dynamic> info = responseData['info'];
           return LastSeen.fromJson(info);
         } else {
-          // print("Key 'info' not found in response.");
           return null;
         }
       } else {
-        // print("Failed to load the rooms info: ${response.statusCode}");
         return null;
       }
     } catch (e) {
-      print("Error occurred: $e");
+      logger.e("Error occurred: $e");
       return null;
     }
   }
 
-//-------------------------------total activememeber subscribe to same room---------------------//
+  //-------------------------------total activememeber subscribe to same room---------------------//
   Future<List<RoomMemberModel>> fetchTotalActiveMember(String roomId) async {
     try {
       Uri url = BackendProperties.activeMemberUri(roomId);
@@ -145,11 +121,11 @@ class ChatRepo {
             users.map((json) => RoomMemberModel.fromJson(json)).toList();
         return members;
       } else {
-        print("Failed to load users, status code: ${response.statusCode}");
+        logger.w("Failed to load users, status code: ${response.statusCode}");
         return [];
       }
     } catch (error) {
-      print("Error occurred while fetching users: $error");
+      logger.e("Error occurred while fetching users: $error");
       return [];
     }
   }
